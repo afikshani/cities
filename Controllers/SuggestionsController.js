bc6cfb7de37efcc41830589045720756c093177f
afikshani <afikshani1@gmail.com>
2021-10-29 18:36:56 +0300

    fix logic in case score above 1

--- Controllers/SuggestionsController.js
+++ Controllers/SuggestionsController.js
@@ -60,7 +60,8 @@ function calculateScoreAfterTextFiltering({ text, latitude: lat, longitude: long
     } else return results.map(item => {
             const diffInLat = findDifference(item.lat, lat);
             const diffInLong = findDifference(item.long, long);
-            item.score = Math.sqrt(Math.pow(calScoreBasedOnDiff(diffInLat), 2) + Math.pow(calScoreBasedOnDiff(diffInLong), 2));
+            const score = Math.sqrt(Math.pow(calScoreBasedOnDiff(diffInLat), 2) + Math.pow(calScoreBasedOnDiff(diffInLong), 2));
+            item.score = score > 1 ? getRandomNumberInRange(QUERY_SCORE_COEFFICIENT.THIRD, EDGE_VALUES_FOR_SCORE.MAX_COEFFICIENT) : score;
             return item;
     });
 }
